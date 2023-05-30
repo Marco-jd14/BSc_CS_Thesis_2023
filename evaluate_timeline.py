73e73e80a4df6f1a4d68f6b3bd475ecac103fe7d
Marco <marco.deken@gmail.com>
2023-05-30 22:57:42 +0200

    Start to checking and evaluating timeline

--- evaluate_timeline.py
+++ evaluate_timeline.py
@@ -4,6 +4,8 @@ Created on Mon May 29 20:12:40 2023
 
 @author: Marco
 """
+import os
+import re
 import sys
 import copy
 import enum
@@ -20,25 +22,90 @@ import database.connect_db as connect_db
 import database.query_db as query_db
 Event = query_db.Event
 
+
 def main():
+
+    export_folder = './timelines/'
+    version_to_read = 2
+
     TrackTime("Connect to db")
     conn = connect_db.establish_host_connection()
     db   = connect_db.establish_database_connection(conn)
     print("Successfully connected to database '%s'"%str(db.engine).split("/")[-1][:-1])
 
-    TrackTime("Read Excel")
-    events_list_path = "./timelines/events_list_2023.05.29-20.03.37.xlsx"
-    events_df = pd.read_excel(events_list_path, "events", index_col="Unnamed: 0")
+    TrackTime("Retrieve from db")
+    result = query_db.retrieve_from_sql_db(db, 'filtered_issues')
+    filtered_issues, = result
+
+    TrackTime("Read pickle")
+    events_df, utility_df = read_input_pickles(2, export_folder)
+    events_df['event'] = events_df['event'].apply(lambda event: Event[str(event)[len("Event."):]])
 
     TrackTime("Evaluate timeline")
+    perform_sense_check(events_df, filtered_issues)
     evaluate_timeline(events_df)
 
     db.close()
     TrackReport()
 
 
+def read_input_pickles(version, export_folder):
+    contents = os.listdir(export_folder)
+    contents = list(filter(lambda name: re.search("^%d_.*\.pkl"%version, name), contents))
+    events_df  = pd.read_pickle(export_folder + (contents[0] if "events_df" in contents[0] else contents[1]))
+    utility_df = pd.read_pickle(export_folder + (contents[0] if "utility_df" in contents[0] else contents[1]))
+    return events_df, utility_df
+
+
 def evaluate_timeline(events_df):
-    print(events_df)
+    events_df
+    # print(events_df)
+
+def perform_sense_check(events_df, issues):
+    relevant_issue_columns_check   = ['id', 'offer_id', 'sent_at', 'amount', 'expires_at']
+    relevant_issue_columns_compare = ['id', 'total_issued', 'nr_accepted', 'nr_expired', 'nr_not_sent_on']
+    issues = issues[relevant_issue_columns_check + relevant_issue_columns_compare[1:]]
+    issues = issues.rename(columns={'id':'issue_id'}).sort_values('issue_id').reset_index(drop=True)
+
+    accepted_events = events_df[events_df['event'] == Event.member_accepted]
+    nr_accepted = accepted_events.groupby('issue_id').aggregate(nr_accepted=('coupon_id','count')).reset_index()
+
+    declined_events = events_df[events_df['event'] == Event.member_declined]
+    nr_declined = declined_events.groupby('issue_id').aggregate(nr_declined=('coupon_id','count')).reset_index()
+
+    let_expire_events = events_df[events_df['event'] == Event.member_let_expire]
+    nr_let_expire = let_expire_events.groupby('issue_id').aggregate(nr_let_expire=('coupon_id','count')).reset_index()
+
+    available_events = events_df[events_df['event'] == Event.coupon_available]
+    first_follow_available = available_events.groupby('coupon_follow_id').first().reset_index()
+    nr_first_available = first_follow_available.groupby('issue_id').aggregate(nr_first_available=('coupon_id','count')).reset_index()
+
+    list_of_tables = [nr_accepted, nr_declined, nr_let_expire, nr_first_available]
+    simulation_results = join_tables(list_of_tables, ['issue_id'], issues[['issue_id']])
+    simulation_results = simulation_results.sort_values(by='issue_id').reset_index(drop=True)
+    print(simulation_results)
+
+    to_compare = simulation_results.merge(issues.add_suffix('_baseline').rename(columns={'issue_id_baseline':'issue_id'}), on='issue_id')
+
+    assert np.all(to_compare['amount_baseline'] == to_compare['nr_first_available'])
+    diff = (to_compare['nr_accepted'] - to_compare['nr_accepted_baseline']).abs()
+    print(np.sum(diff), np.sort(diff.fillna(0))[-10:])
+
+
+
+def join_tables(list_of_tables, join_on, all_ids=None):
+    if all_ids is None:
+        result = list_of_tables[0][join_on]
+    else:
+        print(all_ids.columns)
+        for i, col in enumerate(all_ids.columns):
+            all_ids = all_ids.rename(columns={col:join_on[i]})
+        result = all_ids
+
+    for i, table in enumerate(list_of_tables):
+        result = pd.merge(result, table, on=join_on, how='left')
+
+    return result.convert_dtypes()
 
 
 if __name__ == '__main__':
